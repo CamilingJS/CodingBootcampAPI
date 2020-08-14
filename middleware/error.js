7dc3d511809378b1957630be6e8022db16b01af6
Jonathan <jonathancamiling@gmail.com>
2020-08-13 18:42:37 -0700

    added validation in error.js for duplicate ids and missing fields

--- middleware/error.js
+++ middleware/error.js
@@ -1,24 +1,35 @@
-const ErrorResponse = require("../utils/errorResponse");
+const ErrorResponse = require('../utils/errorResponse');
 
 const errorHandler = (err, req, res, next) => {
-    let error = {...err};
+  let error = { ...err };
 
-    error.message = err.message;
+  error.message = err.message;
 
-    //Log to console for dev
-    console.log(err.stack.red);
+  //Log to console for dev
+  console.log(err);
 
-    //Mongoose bad ObjectID
-    if(err.name === 'CastError') {
-        const message = `Resource not found with id of ${err.value}`;
-        error = new ErrorResponse(message, 404);
+  //Mongoose bad ObjectID
+  if (err.name === 'CastError') {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
 
-    }
+  //Mongoose duplicate key
+  if (error.code === 11000) {
+    const message = 'Dulicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
 
-    res.status( error.statusCode || 500).json({
-        success: false, 
-        error: error.message || 'Server Error'
-    });
+  //Mongoose validation errr
+  if (err.name = 'ValidationError') {
+      const message = Object.values(err.errors).map(val => val.message);
+      error = new  ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || 'Server Error',
+  });
 };
 
 module.exports = errorHandler;
