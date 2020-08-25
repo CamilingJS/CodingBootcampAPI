8104bb3fee4fd4008e65c44a23f775da504d74d2
Jonathan <jonathancamiling@gmail.com>
2020-08-24 18:03:38 -0700

    quick spaciing

--- routes/courses.js
+++ routes/courses.js
@@ -4,6 +4,7 @@ const { getCourses, getCourse, addCourse } = require('../controllers/courses');
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(addCourse);
+
 router.route('/:id').get(getCourse);
 
 module.exports = router;
