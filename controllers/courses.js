cb6ddb7e3b9ddc3e92228025bdb0a2873e174191
Jonathan <jonathancamiling@gmail.com>
2020-09-03 21:44:21 -0700

    cleared comments

--- controllers/courses.js
+++ controllers/courses.js
@@ -95,38 +95,11 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
     runValidators: true
   });
 
-  console.log(req.body.minimumSkill);
-
   res.status(200).json({
     success: true,
     data: course,
   });
 });
-
-//--------------------------------------------------------------------------
-// @desc  Update course
-// @route   PUT /api/v1/courses/:id
-// @access  Private
-// exports.updateCourse = asyncHandler(async (req, res, next) => {
-//   let course = await Course.findById(req.params.id);
-
-//   if (!course) {
-//     return next(
-//       new ErrorResponse(`No course with the id of ${req.params.id}`),
-//       404
-//     );
-//   }
-
-//   course = await Course.findByIdAndUpdate(req.params.id, req.body, {
-//     new: true,
-//     runValidators: true
-//   })
-//   res.status(200).json({
-//     success: true,
-//     data: course,
-//   });
-// });
-
 
 //--------------------------------------------------------------------------
 // @desc  Delete course
